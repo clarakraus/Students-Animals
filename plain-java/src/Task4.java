b66b748b954d8ad782cc6d183239ccb31e08c1aa
André Schreck <moin@andreschreck.de>
2022-05-17 17:10:28 +0200

    task 5

--- plain-java/src/Task4.java
+++ plain-java/src/Task4.java
@@ -1,15 +1,17 @@
 public class Task4 {
 
     public static void main(String[] args) {
-        printSum(add(7, 17));
+        System.out.println(printSum(add(7, 17)));
     }
 
     private static int add(int number1, int number2) {
+        System.out.println("number1 > 100? " + (number1 > 100));
+        System.out.println("number2 > 100? " + (number2 > 100));
         return number1 + number2;
     }
 
-    private static void printSum(int sum) {
-        System.out.println("Summe: " + sum);
+    private static String printSum(int sum) {
+        return "Summe: " + sum;
     }
 
 }
