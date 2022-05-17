663b1b444be527bc46b710d1f17ad535636b2fd2
André Schreck <moin@andreschreck.de>
2022-05-17 15:26:13 +0200

    task 3

--- plain-java/src/Task2.java
+++ plain-java/src/Task2.java
@@ -3,8 +3,7 @@ public class Task2 {
     public static void main(String[] args) {
         int i1 = 7;
         int i2 = 17;
-        var result = i1 + i2;
-        System.out.println("Summe: " + result);
+        System.out.println("Summe: " + (i1 + i2));
     }
 
 }
