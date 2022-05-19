157d5b9f3a266cb20c043f7f9656d0604e5bfe4c
André Schreck <moin@andreschreck.de>
2022-05-19 15:45:46 +0200

    fail

--- myMavenProject/src/main/java/Arrays.java
+++ myMavenProject/src/main/java/Arrays.java
@@ -1,9 +1,13 @@
 public class Arrays {
 
     public static String[] createStudents(int length) {
+        if (length < 0) {
+            return new String[0];
+        }
+
         String[] students = new String[length];
         for (int i = 0; i < students.length; i++) {
-            students[i] = "Student " + i;
+            students[i] = "Student " + (i + 1);
         }
         return students;
     }
