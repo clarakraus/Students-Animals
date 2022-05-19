3f09d2dff5bbec7264e613da364e0442d9a25d70
André Schreck <moin@andreschreck.de>
2022-05-19 15:48:45 +0200

    fixed test

--- myMavenProject/src/test/java/ArraysTest.java
+++ myMavenProject/src/test/java/ArraysTest.java
@@ -11,8 +11,14 @@ class ArraysTest {
     }
 
     @Test
+    void shouldCreateEmptyArrayForNegativeNumber() {
+        String[] expected = new String[0];
+        assertArrayEquals(expected, Arrays.createStudents(-1));
+    }
+
+    @Test
     void shouldCreateMultipleStudents() {
-        String[] expected = {"Student 0", "Student 1", "Student 2"};
+        String[] expected = {"Student 1", "Student 2", "Student 3"};
         assertArrayEquals(expected, Arrays.createStudents(3));
     }
 
